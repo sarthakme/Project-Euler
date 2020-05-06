71472df4715196ba3d16d1f31b8953f4cf2ecb2b
sarthakme <sarthakrocks99@yahoo.com>
2020-05-06 17:52:19 +0530

    Increased readability

--- 1. Multiples of 3 and 5.py
+++ 1. Multiples of 3 and 5.py
@@ -1,10 +1,32 @@
 num = 1000
+
+#For co-prime numbers
 a = 3
 b = 5
+
 p1 = (num - 1) // a
 p2 = (num - 1) // b
 p3 = (num - 1) // (a * b)
+
 n1 = (p1 * (p1 + 1) * a) // 2
 n2 = (p2 * (p2 + 1) * b) // 2
 n3 = (p3 * (p3 + 1) * a * b) // 2
+
 print(n1 + n2 - n3)
+
+'''
+For non-co-prime numbers
+First find LCM
+
+mx = max(a, b)
+mn = min(a, b)
+
+n = mx
+while n <= mx * mn:
+    if n % mn == 0:
+        LCM = n
+        break
+    n += mx
+
+Instead of a * b term, use LCM
+'''
--- 2. Even Fibbonaci Numbers.py
+++ 2. Even Fibbonaci Numbers.py
@@ -1,13 +1,17 @@
 s = 0
 n = 4000000
+
 a = 0
 b = 1
+
 if a % 2 == 0:
     s += a
 if b % 2 == 0:
     s += b
+
 while a + b <= n:
     a, b = a + b, a
     if a % 2 == 0:
         s += a
+
 print(s)
